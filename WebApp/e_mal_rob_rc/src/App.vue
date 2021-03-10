3d57eee00d8a694a70ed18193eec1107d3c4bdef
Alexander <dudalex1@gmail.com>
2021-03-10 16:37:37 +0100

    keyboard input working

--- WebApp/e_mal_rob_rc/src/App.vue
+++ WebApp/e_mal_rob_rc/src/App.vue
@@ -1,30 +1,17 @@
 <template>
   <h1>eMalRob Remote Control</h1>
-  <Inputs />
-  <div class="container">
-    <div class="row">
-      <form class="col s12 m8 l8 xl8">
-        <div class="row">
-          <div class="input-field col s12 m9 l6 xl6 offset-xl5 offset-l5 offset-m2">
-            <i class="material-icons prefix">settings_ethernet</i>
-            <input id="icon_prefix" type="text" />
-            <label for="icon_prefix">Websocket Adress</label>
-          </div>
-          <div class="input-field col s1 m1 l1 xl1">
-            <a class="waves-effect waves-light btn inline blue darken-1">Connect</a>
-          </div>
-        </div>
-      </form>
-    </div>
-  </div>
+  <Keys />
+  <Form />
 </template>
 <script>
-import Inputs from "./components/Inputs";
+import Keys from "./components/Keys";
+import Form from "./components/Form";
 
 export default {
   name: "App",
   components: {
-    Inputs
+    Keys,
+    Form
   }
 };
 </script>
---
+++ WebApp/e_mal_rob_rc/src/components/Keys.vue
@@ -0,0 +1,105 @@
+<template>
+  <link
+    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+    rel="stylesheet"
+  />
+  <button
+    class="kbc-button"
+    v-bind:class="{ active: upArrow }"
+    @click="ButtonClick('Up')"
+  >
+    <span class="material-icons">
+      keyboard_arrow_up
+    </span>
+  </button>
+  <br />
+  <button
+    class="kbc-button"
+    v-bind:class="{ active: leftArrow }"
+    @click="ButtonClick('Left')"
+  >
+    <span class="material-icons">
+      keyboard_arrow_left
+    </span>
+  </button>
+  <button
+    class="kbc-button"
+    v-bind:class="{ active: downArrow }"
+    @click="ButtonClick('Down')"
+  >
+    <span class="material-icons">
+      keyboard_arrow_down
+    </span>
+  </button>
+  <button
+    class="kbc-button"
+    v-bind:class="{ active: rightArrow }"
+    @click="ButtonClick('Right')"
+  >
+    <span class="material-icons">
+      keyboard_arrow_right
+    </span>
+  </button>
+  <br /><br /><br />
+</template>
+
+<script>
+export default {
+  created() {
+    window.addEventListener("keydown", this.ArrowKeys);
+    window.addEventListener("keyup", this.ArrowKeys);
+  },
+  name: "Keys",
+  data() {
+    return {
+      leftArrow: false,
+      upArrow: false,
+      rightArrow: false,
+      downArrow: false
+    };
+  },
+  methods: {
+    ButtonClick(button) {
+      console.log(button);
+    },
+    ArrowKeys(event) {
+      switch (event.key) {
+        case "Up":
+        case "ArrowUp":
+          if (event.type == "keydown") {
+            this.upArrow = true;
+          } else {
+            this.upArrow = !this.upArrow;
+          }
+          break;
+        case "Left":
+        case "ArrowLeft":
+          if (event.type == "keydown") {
+            this.leftArrow = true;
+          } else {
+            this.leftArrow = !this.leftArrow;
+          }
+          break;
+        case "Down":
+        case "ArrowDown":
+          if (event.type == "keydown") {
+            this.downArrow = true;
+          } else {
+            this.downArrow = !this.downArrow;
+          }
+          break;
+        case "Right":
+        case "ArrowRight":
+          if (event.type == "keydown") {
+            this.rightArrow = true;
+          } else {
+            this.rightArrow = !this.rightArrow;
+          }
+          break;
+      }
+    }
+  }
+};
+</script>
+
+<style lang="sass"></style>
