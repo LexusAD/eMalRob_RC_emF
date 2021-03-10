93098a2b206235a34b0fae69acd7ae6add5d58c0
Alexander <dudalex1@gmail.com>
2021-03-10 04:01:15 +0100

    update git ignore

--- WebApp/e_mal_rob_rc/.history/src/main_20210310020759.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import 'keyboard-css';
-import 'material-components-web';
-
-createApp(App).mount("#app");
--- WebApp/e_mal_rob_rc/.history/src/main_20210310021120.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import 'keyboard-css';
-
-createApp(App).mount("#app");
