d53a298e350f903fb81dd471f4f4c24622dfcb2d
Alexander Duda <dudalex1@gmail.com>
2021-05-21 04:27:14 +0200

    WebApp v1.0

--- WebApp/rc_app/src/App.vue
+++ WebApp/rc_app/src/App.vue
@@ -6,23 +6,23 @@
     </div>
     <div class="row">
       <div class="col s12">  
-        <button class="kbc-button" v-bind:class="{ active: upArrow }" @click="ButtonClick('Up')">
+        <button class="kbc-button" v-bind:class="{ active: upArrow }" @mousedown="ButtonPressed('Up')" @click="ButtonClicked('Up')">
           <span class="material-icons">
             keyboard_arrow_up
           </span>
         </button>
         <br />
-        <button class="kbc-button" v-bind:class="{ active: leftArrow }" @click="ButtonClick('Left')">
+        <button class="kbc-button" v-bind:class="{ active: leftArrow }" @mousedown="ButtonPressed('Left')" @click="ButtonClicked('Left')">
           <span class="material-icons">
             keyboard_arrow_left
           </span>
         </button>
-        <button class="kbc-button" v-bind:class="{ active: downArrow }" @click="ButtonClick('Down')">
+        <button class="kbc-button" v-bind:class="{ active: downArrow }" @mousedown="ButtonPressed('Down')" @click="ButtonClicked('Down')">
           <span class="material-icons">
             keyboard_arrow_down
           </span>
         </button>
-        <button class="kbc-button" v-bind:class="{ active: rightArrow }" @click="ButtonClick('Right')">
+        <button class="kbc-button" v-bind:class="{ active: rightArrow }" @mousedown="ButtonPressed('Right')" @click="ButtonClicked('Right')">
           <span class="material-icons">
             keyboard_arrow_right
           </span>
@@ -40,13 +40,15 @@
           </div>
       </form>
       <div class="col m1 l1">
-          <a v-on:click="connect" class="waves-effect waves-light btn inline blue darken-1 hide-on-small-only">Connect</a>
+          <a v-show="!connected" class="waves-effect waves-light btn inline blue darken-1 hide-on-small-only" @click="connect()">Connect</a>
+          <a v-show="connected" class="waves-effect waves-light btn inline  grey darken-1 hide-on-small-only" @click="disconnect()">Disconnect</a>
       </div>
       <div class="col m3 l3"></div>
     </div>
     <div class="row">
       <div class="col s12">
-          <a v-on:click="connect" class="waves-effect waves-light btn inline blue darken-1 hide-on-med-and-up">Connect</a>
+          <a v-show="!connected" class="waves-effect waves-light btn inline blue darken-1 hide-on-med-and-up" @click="connect()">Connect</a>
+          <a v-show="connected" class="waves-effect waves-light btn inline  grey darken-1 hide-on-med-and-up" @click="disconnect()">Disconnect</a>
       </div>
     </div>
   </div>  
@@ -62,7 +64,9 @@ export default {
   },
   name: "Keys",
   data() {
+    
     return {
+      connected: false,
       leftArrow: false,
       upArrow: false,
       rightArrow: false,
@@ -72,48 +76,89 @@ export default {
     };
   },
   methods: {
-    ButtonClick(button) {
-      console.log(button);
+    ButtonPressed(button) {
+      switch (button) {
+        case "Up":
+            console.log("MOUSE: Up arrow pressed");
+            this.ws_instance.send("U1");
+          break;
+        case "Left":
+            console.log("MOUSE: Left arrow pressed");
+            this.ws_instance.send("L1");
+          break;
+        case "Down":
+            console.log("MOUSE: Down arrow pressed");
+            this.ws_instance.send("D1");
+          break;
+        case "Right":
+            console.log("MOUSE: Right arrow pressed");
+            this.ws_instance.send("R1");
+          break;
+      }
+    },
+    ButtonClicked(button) {
+      switch (button) {
+        case "Up":
+            console.log("MOUSE: Up arrow released");
+            this.ws_instance.send("U0");
+          break;
+        case "Left":
+            console.log("MOUSE: Left arrow released");
+            this.ws_instance.send("L0");
+          break;
+        case "Down":
+            console.log("MOUSE: Down arrow released");
+            this.ws_instance.send("D0");
+          break;
+        case "Right":
+            console.log("MOUSE: Right arrow released");
+            this.ws_instance.send("R0");
+          break;
+      }
     },
     ArrowKeys(event) {
       switch (event.key) {
-        case "Up":
         case "ArrowUp":
             if (event.type == "keydown" && this.upArrow != true) {
-              console.log("Up Arrow Down");
+              console.log("KEYBOARD: Up arrow pressed");
+              this.ws_instance.send("U1");
               this.upArrow = true;
             } else if (event.type == "keyup" && this.upArrow == true) {
-              console.log("Up Arrow Released");
+              console.log("KEYBOARD: Up arrow released");
+              this.ws_instance.send("U0");
               this.upArrow = false;
             }
           break;
-        case "Left":
         case "ArrowLeft":
             if (event.type == "keydown" && this.leftArrow != true) {
-              console.log("left Arrow Down");
+              console.log("KEYBOARD: Left arrow pressed");
+              this.ws_instance.send("L1");
               this.leftArrow = true;
             } else if (event.type == "keyup" && this.leftArrow == true) {
-              console.log("left Arrow Released");
+              console.log("KEYBOARD: Left arrow released");
+              this.ws_instance.send("L0");
               this.leftArrow = false;
             }
           break;
-        case "Down":
         case "ArrowDown":
             if (event.type == "keydown" && this.downArrow != true) {
-              console.log("down Arrow Down");
+              console.log("KEYBOARD: Down arrow pressed");
+              this.ws_instance.send("D1");
               this.downArrow = true;
             } else if (event.type == "keyup" && this.downArrow == true) {
-              console.log("down Arrow Released");
+              console.log("KEYBOARD: Down arrow released");
+              this.ws_instance.send("D0");
               this.downArrow = false;
             }
           break;
-        case "Right":
         case "ArrowRight":
             if (event.type == "keydown" && this.rightArrow != true) {
-              console.log("right Arrow Down");
+              console.log("KEYBOARD: Right arrow pressed");
+              this.ws_instance.send("R1");
               this.rightArrow = true;
             } else if (event.type == "keyup" && this.rightArrow == true) {
-              console.log("right Arrow Released");
+              console.log("KEYBOARD: Right arrow released");
+              this.ws_instance.send("R0");
               this.rightArrow = false;
             }
           break;
@@ -122,19 +167,23 @@ export default {
     connect() {
       console.log("Connecting ...")
       this.ws_instance = new WebSocket(this.socket_url)
-      
-      this.ws_instance.onmessage = function(event) {
-        console.log(event);
+      this.ws_instance.onmessage = function(msg) {
+        console.log(msg);
       }
-
-      this.ws_instance.onopen = function(event) {
-        console.log(event)
-        console.log("Successfully connected to the echo websocket server...")
+      this.ws_instance.onopen = function(msg) {
+        console.log(msg)  
+        console.log("Successfully connected to websocket server...")
       }
+      this.connected = true;
     },
-    sendKey(key, state) {
-      console.log(this.ws_instance);
-      this.ws_instance.send(message);
+    disconnect() {
+      
+      this.ws_instance.close();
+      this.ws_instance.onclose = function(msg) {
+        console.log(msg)
+        console.log("Successfully disconnected from websocket server...")
+      }
+      this.connected = false;
     }
   }
 };
